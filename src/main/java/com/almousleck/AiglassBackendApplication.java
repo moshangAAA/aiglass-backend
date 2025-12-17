834c70a7ca766914b5096aca2f34bd16a1f6ee8b
ataousCode <microservicestech97@gmail.com>
2025-12-17 14:48:12 +0800

    feat: Initial project setup
    - Initializes a new Spring Boot application for aiglass-backend.
    - Includes basic project structure with Maven.
    - Adds initial configuration for application properties, docker-compose, and .gitignore.

---
+++ src/main/java/com/almousleck/AiglassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.almousleck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiglassBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiglassBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/almousleck/AiglassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almousleck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiglassBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
