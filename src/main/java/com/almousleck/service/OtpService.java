8a564e5a4809c9c1b87d59da8ef22da509bfbcc2
ataousCode <microservicestech97@gmail.com>
2025-12-22 16:25:19 +0800

    refactor: remove unused OtpService and OtpServiceImpl OTP logic is implemented directly in AuthenticationServiceImpl for better cohesion with authentication flow

--- src/main/java/com/almousleck/service/OtpService.java
+++
@@ -1,9 +0,0 @@
-package com.almousleck.service;
-
-import com.almousleck.dto.OtpResponse;
-
-public interface OtpService {
-    OtpResponse generateOtp(String phoneNumber);
-    void verifyOtp(String phoneNumber, String optCode);
-    OtpResponse resendOpt(String phoneNumber);
-}
